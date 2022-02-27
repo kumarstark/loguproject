18a7257b12d2e9d82cc52211b5095e9f596751d9
kumarstark <sathish4live@gmail.com>
2022-02-27 02:57:00 -0500

    ab work space

--- src/org/deepu/Speed.java
+++ src/org/deepu/Speed.java
@@ -5,7 +5,8 @@ public class Speed {
 	public static void main(String[] args) {
 		
 		System.out.println("deeksha logu");
-		
+		System.out.println("hiiii");
+		System.out.println("hello world");
 		
 		
 		
