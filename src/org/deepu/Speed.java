0dfa7096d85e3b1cc98705ca53befee1c27c23e3
kumarstark <sathish4live@gmail.com>
2022-02-27 02:20:38 -0500

    my love

---
+++ src/org/deepu/Speed.java
@@ -0,0 +1,26 @@
+package org.deepu;
+
+public class Speed {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("deeksha logu");
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
